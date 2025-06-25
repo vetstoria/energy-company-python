668f2a193e96824b0ce82804abb68683d07f65be
Siyath <siyath.pandigama@petvisor.com>
2025-06-25 12:39:48 +0530

    refactor tests for electricity reading controller and repository to use pytest

--- tests/service/test_ElectricityReadingService.py
+++ tests/service/test_ElectricityReadingService.py
@@ -1,33 +1,29 @@
-from unittest import TestCase
-from unittest.mock import MagicMock
-
+import pytest
+from unittest.mock import AsyncMock
 from src.domain.electricity_reading import ElectricityReading
-from src.repository.electricity_reading_repository import ElectricityReadingRepository
 from src.service.electricity_reading_service import ElectricityReadingService
 from src.service.time_converter import iso_format_to_unix_time
 
 
-class TestElectricityReadingService(TestCase):
-    def setUp(self):
-        self.repository = ElectricityReadingRepository()
-        self.repository.store = MagicMock()
-        self.electricity_reading_service = ElectricityReadingService(self.repository)
+@pytest.mark.asyncio
+async def test_calls_repository_to_store_readings():
+    repo_mock = AsyncMock()
+    service = ElectricityReadingService(repo_mock)
 
-    def test_call_repository_to_store_readings(self):
-        json = {
-            "smartMeterId": "meter-45",
-            "electricityReadings": [
-                {"time": iso_format_to_unix_time("2015-03-02T08:55:00"), "reading": 0.812},
-                {"time": iso_format_to_unix_time("2015-09-02T08:55:00"), "reading": 0.23},
-            ],
-        }
+    payload = {
+        "smartMeterId": "meter-45",
+        "electricityReadings": [
+            {"time": iso_format_to_unix_time("2015-03-02T08:55:00"), "reading": 0.812},
+            {"time": iso_format_to_unix_time("2015-09-02T08:55:00"), "reading": 0.23},
+        ],
+    }
 
-        self.electricity_reading_service.store_reading(json)
+    await service.store_reading(payload)
 
-        self.repository.store.assert_called_with(
-            "meter-45",
-            [
-                ElectricityReading({"time": iso_format_to_unix_time("2015-03-02T08:55:00"), "reading": 0.812}),
-                ElectricityReading({"time": iso_format_to_unix_time("2015-09-02T08:55:00"), "reading": 0.23}),
-            ],
-        )
+    repo_mock.store.assert_awaited_once_with(
+        "meter-45",
+        [
+            ElectricityReading({"time": iso_format_to_unix_time("2015-03-02T08:55:00"), "reading": 0.812}),
+            ElectricityReading({"time": iso_format_to_unix_time("2015-09-02T08:55:00"), "reading": 0.23}),
+        ],
+    )
